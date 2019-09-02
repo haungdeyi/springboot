896ac49b2efba9122c929885a057826e92693b2d
huangdeyi <1534527968@qq.com>
2019-09-02 08:58:58 +0800

    commit in the git client

--- SpringBoot/src/main/java/com/springboot/application/Application.java
+++ SpringBoot/src/main/java/com/springboot/application/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	//启动运用
-	public static void main(String[] agrs) {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
 }
